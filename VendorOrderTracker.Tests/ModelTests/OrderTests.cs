19e90689a2031ed62f87c825f1359a6407e549b9
Matt Stroud <lixingke06@gmail.com>
2020-05-22 09:31:25 -0700

    add test for Constructor to create instance of Order

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,6 +6,10 @@ namespace VendorOrderTracker.Tests
   [TestClass]
   public class OrderTests
   {
-
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test", "test", "test", "test");
+    }
   }
 }
