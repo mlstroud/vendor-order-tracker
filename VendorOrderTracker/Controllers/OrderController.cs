90154e47ddd7aac229c9078180074dd02c1d42d0
Matt Stroud <lixingke06@gmail.com>
2020-05-22 14:14:40 -0700

    update route for showing orders

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -1,13 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Controllers
 {
   public class OrderController : Controller
   {
     [HttpGet("/vendors/{id}/orders")]
-    public ActionResult Index()
+    public ActionResult Index(int id)
     {
-      return View();
+      List<Order> orders;
+      Vendor vendor = Vendor.FindVendor(id);
+      if (vendor.Orders != null)
+      {
+        orders = vendor.Orders;
+      }
+      else
+      {
+        orders = new List<Order>();
+      }
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("vendor", vendor);
+      model.Add("orders", orders);
+      return View(model);
+    }
+
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult New(int id)
+    {
+      Vendor vendor = Vendor.FindVendor(id);
+      return View(vendor);
     }
   }
 }
