e27c5b992885b035889fb637d5e91b51e587208f
Matt Stroud <lixingke06@gmail.com>
2020-05-22 08:56:56 -0700

    update Constructor to store Name

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor>();
+    public string Name { get; set; }
+    public string Description { get; set; }
+    private static int IDCount { get; set; }
+    public int Id { get; set; }
+    public List<Order> Orders { get; set; }
 
+    public Vendor(string name, string description)
+    {
+      Name = name;
+    }
   }
 }
