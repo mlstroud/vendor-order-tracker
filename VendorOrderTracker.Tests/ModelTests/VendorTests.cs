9474ab2ec1c23a5c3026b1c3efe1b338effb7383
Matt Stroud <lixingke06@gmail.com>
2020-05-22 08:54:34 -0700

    add test for Constructor creating instance of Vendor

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,11 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
+using System;
 
 namespace VendorOrderTracker.Tests
 {
   [TestClass]
   public class VendorTests
   {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      string name = "test";
+      string description = "test";
+      Vendor newVendor = new Vendor(name, description);
 
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }
