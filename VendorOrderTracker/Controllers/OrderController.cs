83917a2c35f485789636d69fc76ace11522f37e6
Matt Stroud <lixingke06@gmail.com>
2020-05-22 14:49:00 -0700

    update routing for Create to create order and redirect to index

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -9,20 +9,21 @@ namespace VendorOrderTracker.Controllers
     [HttpGet("/vendors/{id}/orders")]
     public ActionResult Index(int id)
     {
-      List<Order> orders;
       Vendor vendor = Vendor.FindVendor(id);
-      if (vendor.Orders != null)
-      {
-        orders = vendor.Orders;
-      }
-      else
-      {
-        orders = new List<Order>();
-      }
+      List<Order> orders = vendor.Orders;
       Dictionary<string, object> model = new Dictionary<string, object>();
       model.Add("vendor", vendor);
       model.Add("orders", orders);
       return View(model);
+    }
+
+    [HttpPost("/vendors/{id}/orders")]
+    public ActionResult Create(int id, string title, string description, string price, string date)
+    {
+      Order order = new Order(title, description, price, date);
+      Vendor vendor = Vendor.FindVendor(id);
+      vendor.AddOrder(order);
+      return RedirectToAction("Index");
     }
 
     [HttpGet("/vendors/{id}/orders/new")]
