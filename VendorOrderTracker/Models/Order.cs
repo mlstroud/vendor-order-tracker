373354d97c10bf783e1c94ccc0b13832bbe76da0
Matt Stroud <lixingke06@gmail.com>
2020-05-22 09:36:55 -0700

    update Constructor to generate unique ID for each instance

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+
 namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    private static List<Order> _instances = new List<Order>();
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Date { get; set; }
+    private static int IDCount { get; set; }
+    public int Id { get; set; }
 
+    public Order(string title, string description, string price, string date)
+    {
+      IDCount++;
+      Id = IDCount;
+    }
   }
 }
